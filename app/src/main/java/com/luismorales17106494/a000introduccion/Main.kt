2808e23923b991effacb9c82e2db8736b5b69b39
Edu-Lu <moralesluis17106494@gmail.com>
2022-07-19 11:20:20 -0500

    Se agrego la clase 001 Variables

--- app/src/main/java/com/luismorales17106494/a000introduccion/Main.kt
+++ app/src/main/java/com/luismorales17106494/a000introduccion/Main.kt
@@ -2,6 +2,21 @@ package com.luismorales17106494.a000introduccion
 
 fun main() {
 
+    clase001Variables()
+}
+
+/*001 Variables*/
+fun clase001Variables() {
+    //Java
+    //final Int a = 2;
+    //Int b = 2;
+    //int c = a + b;
+
+    //Kotlin
+    val a: Int = 2
+    var b: Int = 2
+    var c: Int = a + b
+    println(c)
 
 
 }
